18e5f12c30faa697e136194ba1697013f0a9f0ef
Franklin <franklinjaradev@gmail.com>
2024-09-04 17:00:44 -0400

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     const stdin = std.io.getStdIn().reader();
     var buffer: [1024]u8 = undefined;
+    const commands = [_][]const u8{"echo"};
 
     while (true) {
         try stdout.print("$ ", .{});
@@ -12,7 +13,28 @@ pub fn main() !void {
             if (std.mem.eql(u8, user_input, "exit 0")) {
                 std.process.exit(0);
             }
-            try stdout.print("{s}: command not found\n", .{user_input});
+
+            var command_found = false;
+            var command_split = std.mem.splitSequence(u8, user_input, " ");
+            if (command_split.next()) |first_word| {
+                for (commands) |command| {
+                    if (std.mem.eql(u8, first_word, command)) {
+                        command_found = true;
+                        if (std.mem.eql(u8, command, "echo")) {
+                            const echo_content = user_input[5..];
+                            if (echo_content.len == 0) {
+                                try stdout.print("echo: missing argument\n", .{});
+                            } else {
+                                try stdout.print("{s}\n", .{echo_content});
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+            if (!command_found) {
+                try stdout.print("{s}: command not found\n", .{user_input});
+            }
         }
     }
 }
